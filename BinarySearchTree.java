cf5fa8107569244c8052a6bb0009824c40b8c438
icycoldveins <81425589+icycoldveins@users.noreply.github.com>
2022-03-19 19:42:13 -0400

    first commit

---
+++ BinarySearchTree.java
@@ -0,0 +1,13 @@
+public class BinarySearchTree<E extends Comparable<E>>{
+    private int size;
+    private Node<E> root;
+
+private static class Node<E>{
+    private E item;
+    private Node<E>left;
+    private Node<E> right;
+}
+public static void main(String[] args) {
+    
+}
+}
